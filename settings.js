13fafed196e1245514789247587267c870c359b5
Vanessa-christelle13 <akissin815@gmail.com>
2025-03-09 17:34:58 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~Yt5T3QTI#aQc_KwzzEpGdk0GxkBvat2ajOuzysVHHIp-sRJAc7F0",
+SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~AsI1mSbS#ICv5_pTOzEjKGWn4KjsWNkDuXQFum5yqpWYh1rcdCOs",
 // add your Session Id ,session id starts with MALVIN-XD~
 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
 
-BOT_NAME: process.env.BOT_NAME || "✦flamme",
+BOT_NAME: process.env.BOT_NAME || "✦Node.js CI",
 // add bot name here for menu
 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
@@ -26,7 +26,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "2250501009677",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "2250502555012",
 // add your bot owner number
 
 OWNER_NAME: process.env.OWNER_NAME || "✦flamme🪀✦",
